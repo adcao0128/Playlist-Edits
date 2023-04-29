e253d72a10850a1f8289753c99545001c07d70c7
adcao0128 <adc135@case.edu>
2023-04-28 22:22:31 -0400

    Initial Commit 2

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,25 +1,15 @@
 <template>
-  <Login msg="Welcome to Your Vue.js App"/>
+  <LoginServices />
 </template>
 
 <script>
-import Login from './components/Login.vue'
+import LoginServices from './components/LoginServices.vue'
 
 export default {
   name: 'App',
   components: {
-    Login
+    LoginServices
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
