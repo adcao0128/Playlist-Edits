95b1a34eabb9dd7fe8fb1990660602fa073e38bc
adcao0128 <adc135@case.edu>
2023-04-28 22:31:37 -0400

    Initial commit 4

--- server/index.js
+++ server/index.js
@@ -7,6 +7,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+if(process.env.NODE_ENV === 'production') {
+
+    app.use(express.static(__dirname + '/public/'));
+
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
