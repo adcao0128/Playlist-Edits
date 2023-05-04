fa058a1c85d36099a53eb8423327d514b923b781
adcao0128 <adc135@case.edu>
2023-05-03 21:46:55 -0400

    Final Push

--- client/src/LastFMService.js
+++ client/src/LastFMService.js
@@ -2,17 +2,18 @@ const lastfmurl = 'http://ws.audioscrobbler.com/2.0?';
 const lastfmkey = require('./config.js').lastfm();
 const spotifyID = require('./config.js').clientID();
 const spotifySecret = require('./config.js').getSecret();
-const buffer = require('buffer/').Buffer;
+const Buffer = require('buffer').Buffer;
 
 import $ from "jquery";
 
 class LastFMService {
-
+    //A method to return all the tracks a user has listend to from last fm;
     static async getAllTracks(user) {
         try {
             let currentPage = 1;
             let totalPages = 1;
             let userTrackList = [];
+            //A loop to call the api for every page of songs.
             while (currentPage <= totalPages) {
                 const response = await $.ajax({
                     url: lastfmurl,
@@ -28,86 +29,274 @@ class LastFMService {
                 userTrackList = userTrackList.concat(response.recenttracks.track);
                 totalPages = parseInt(response.recenttracks['@attr'].totalPages);
                 currentPage++;
-                setTimeout(() => {console.log(currentPage)}, 100);
+                setTimeout(() => {console.log(currentPage)}, 250);
             }
             return userTrackList;
         }  catch (error) {
             console.log(error);
         }
     }
-    static async getAllSongs(userTrackList) {
-        return userTrackList.map((e)=>e.name);
-    }
-    /*
-    static async getTopSongs(user) {
+    //A method to get all the tracks a user has listened to in the past month
+    static async getAllPastMonthTracks(user) {
         try {
-            const response = await $.ajax({
-                url: lastfmurl,
-                data: {
-                    method: "user.gettoptracks",
-                    user: `${user}`,
-                    period: "12month",
-                    api_key: lastfmkey,
-                    format: "json"
-                }
-            });
-            console.log(response.toptracks.track);
-            //return response;    //const spotifykey = require('./config').spotify();
-        } catch (error) {
+            //Gets the date from a month ago to pass to the api
+            let today = new Date();
+            let pastMonth = new Date();
+            pastMonth.setMonth(today.getMonth() - 1);
+            let pastMonthInUnix = Math.floor(pastMonth.getTime() / 1000);
+            let currentPage = 1;
+            let totalPages = 1;
+            let userTrackList = [];
+            //A loop to call the api for the past month's tracks
+            while (currentPage <= totalPages) {
+                const response = await $.ajax({
+                    url: lastfmurl,
+                    data: {
+                        method: "user.getrecenttracks",
+                        limit: "200",
+                        user: `${user}`,
+                        page: `${currentPage}`,
+                        from: `${pastMonthInUnix}`,
+                        api_key: lastfmkey,
+                        format: "json"
+                    }
+                });
+                userTrackList = userTrackList.concat(response.recenttracks.track);
+                totalPages = parseInt(response.recenttracks['@attr'].totalPages);
+                currentPage++;
+                setTimeout(() => {console.log(currentPage)}, 250);
+            }
+            return userTrackList;
+        }  catch (error) {
             console.log(error);
         }
     }
-    */
-    
-    static async getSongFrequency(user){
-        let songList = await this.getAllSongs(await this.getAllTracks(user));
+    //A method to map a tracklist to song names used in all time scenarios
+    static async getAllSongs(userTrackList) {
+        return userTrackList.map((e)=>e.name);
+    }
+    //A method to map a tracklist to song names used in past month scenarios
+    static async getPastMonthSongs(userTrackList){
+        return userTrackList.map((e)=>e.name);
+    }
+    //A method to get the frequency of songs given all user tracks
+    static async getAllTimeSongFrequency(user){
+        let songList = await this.getAllSongs(await this.getAllTracks(user)); //This call to get tracks is different
         let songFrequency = [];
+        //A loop for the song frequency
         for(const song of songList){
             songFrequency[song] = (songFrequency[song] || 0) + 1;
         }
         return songFrequency;
     }
-    static async getAccessToken(){
-        const credentials = buffer.from(`${spotifyID}:${spotifySecret}`,'base64').toString('base64');
-        const response = await $.ajax({
-            url: 'https://accounts.spotify.com/api/token', 
-            data: {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    'Authorization': `Basic ${credentials}`
-                },
-                body: 'grant_type=client_credentials',
-            }
-        });
-        return response.access_token;
+    //A method to get the frequency of songs given a past month of user tracks
+    static async getPastMonthSongFrequency(user){
+        let songList = await this.getPastMonthSongs(await this.getAllPastMonthTracks(user)); //This call to get tracks is different
+        let songFrequency = [];
+        //A loop for song frequency
+        for(const song of songList){
+            songFrequency[song] = (songFrequency[song] || 0) + 1;
+        }
+        return songFrequency;
     }
+    //A method to get a Spotify access token using an api call
+    static async getAccessToken(){
+        const response = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',          
+            headers: {
+                'Content-Type' : 'application/x-www-form-urlencoded',
+                'Authorization' : 'Basic ' + Buffer.from(`${spotifyID}:${spotifySecret}`).toString('base64')
+            },
+            body: 'grant_type=client_credentials'
+        });
+        const tokenData = await response.json();
+        return tokenData.access_token;
+    }
+    //A method to get a user's spotify playist given a link using an api call
     static async getSpotifyPlayList(spotifyPlayList){
-        try {        
+        try {
+            //splits the link
             let spotifyLink = spotifyPlayList;         
             let splittedLink = spotifyLink.split("/");
             let playlistID = splittedLink[splittedLink.length - 1];
-            const response = await $.ajax({
-                url: `https://api.spotify.com/v1/playlists/${playlistID}/tracks`,
+            let token = await this.getAccessToken();
+            const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistID}`, {
+                method: 'GET',
                 headers: {
-                    'Authorization': `Bearer ${await this.getAccessToken()}`
-                }
+                    'Authorization' : `Bearer ${token}`
+                },
             });
-            let playlist = response.items.map((n)=>n.track.name);
+            let playlistData = await response.json();
+            let playlist = playlistData.tracks.items.map((i)=>[i.track.name, i.added_at]); //We only want the track name and when it was added to the playlist
             return playlist;
         } catch (error) {
             console.log(error);
         }
     }
-    static async makeSuggestions(lastfmUserName, spotifyPlayListLink){
+    //A method to only get the song frequencies that are in a given playlist
+    static async getPlaylistFrequency(playlist, totalTrackList){
+        for(const index in playlist){
+            //Only add it to the playlist if it is included
+            if(Object.keys(totalTrackList).includes(playlist[index][0])){
+                //Gives the song name, the date added, and its frequency
+                playlist[index] = [playlist[index][0], new Date(`${playlist[index][1]}`), totalTrackList[playlist[index][0]]]; 
+            }
+        }
+        return playlist;
+    }
+    //A method to remove the half most recent additions to a playlist
+    static async removeRecentAdditions(playlist){
+        //Sort by date
+        const songSorter = (a, b) => {
+          return b[1] - a[1];
+        };
+        playlist.sort(songSorter);
+        for(let i = 0; i < (playlist.length / 2); i++){
+            playlist.shift();
+        }
+        return playlist;
+    }
+    //A method to sort by a song's frequency, by ascending order
+    static async sortByFrequency(playlist){
+        const songSorter = (a, b) => {
+          return a[2] - b[2];
+        };
+        playlist.sort(songSorter);
+        return playlist;
+    }
+    //A method to sort by a song's frequency by descending order
+    static async sortByMostFrequency(playlist){
+        const songSorter = (a, b) => {
+          return b[2] - a[2];
+        };
+        playlist.sort(songSorter);
+        return playlist;
+    }
+    //A method to give suggestions. Only want to give the first set of songs.
+    static async getSuggestions(playlist) {
+        let leastPlayed = playlist.slice(0, playlist.length / 4);
+        return leastPlayed;
+    }  
+    //A method to get the all time tracks, and the spotify playlist associated
+    static async getAllTimeLists(lastfmUserName, spotifyPlayListLink){
         try {
             let spotifyPlayList = await this.getSpotifyPlayList(spotifyPlayListLink);
-            let songFrequency = await this.getSongFrequency(lastfmUserName);
-            return [spotifyPlayList, songFrequency];
+            let songFrequency = await this.getAllTimeSongFrequency(lastfmUserName);
+            return [spotifyPlayList, songFrequency]; //Return in order of spotify, then lastfm tracks
         } catch (error) {
             console.log(error);
         }
 
+    }
+    //A method to get the past month tracks with a spotify list associated
+    static async getPastMonthLists(lastfmUserName, spotifyPlayListLink){
+        try {
+            let spotifyPlayList = await this.getSpotifyPlayList(spotifyPlayListLink);
+            let songFrequency = await this.getPastMonthSongFrequency(lastfmUserName);
+            return [spotifyPlayList, songFrequency]; //Return in order of spotify, then lastfm tracks
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
+    //A method to sort a playlist track by when it was added, most recent to oldest
+    static async sortByLatestAddition(playlist){
+        const songSorter = (a, b) => {
+          return a[1] - b[1];
+        };
+        playlist.sort(songSorter);
+        return playlist;
+    }
+    //A method to sort a playlist track by when it was added, oldest to most recent
+    static async removeLatestAddition(playlist) {
+        const songSorter = (a, b) => {
+            return a[1] - b[1];
+        };
+        playlist.sort(songSorter);
+        for(let i = 0; i < (playlist.length / 2); i++){
+            playlist.shift();
+        }
+        return playlist;
+    }
+    //A method to give edit out suggestions for the oldest songs in the playlist, based on what is least listened to all time
+    static async editOutOld(lists){
+        let spotifyPlayList = lists[0];
+        let lastfm = lists[1];
+        let playlist = await this.getPlaylistFrequency(spotifyPlayList, lastfm);
+        playlist = await this.removeRecentAdditions(playlist);
+        playlist = await this.sortByFrequency(playlist);
+        playlist = await this.getSuggestions(playlist);
+        return playlist;
+    }
+    //A method to give edit out suggestions for the most recent songs in the playlist, based on what is least listened to all time
+    static async editOutRecent(lists){
+        let spotifyPlayList = lists[0];
+        let lastfm = lists[1];
+        let playlist = await this.getPlaylistFrequency(spotifyPlayList, lastfm);
+        playlist = await this.removeLatestAddition(playlist);
+        playlist = await this.sortByFrequency(playlist);
+        playlist = await this.getSuggestions(playlist);
+        return playlist;
+    }
+    //A method to give edit out suggestions for the oldest songs in the playlist, based on what is least listened to in the past month
+    static async oneEditOutOld(lists){
+        let spotifyPlayList = lists[0];
+        let lastfm = lists[1];
+        let playlist = await this.getPlaylistFrequency(spotifyPlayList, lastfm);
+        playlist = await this.removeRecentAdditions(playlist);
+        playlist = await this.sortByFrequency(playlist);
+        playlist = await this.getSuggestions(playlist);
+        return playlist;
+    }
+    //A method to give edit out suggestions for the most recently added songs in the playlist, based on what is least listened to in the past month
+    static async oneEditOutRecent(lists){
+        let spotifyPlayList = lists[0];
+        let lastfm = lists[1];
+        let playlist = await this.getPlaylistFrequency(spotifyPlayList, lastfm);
+        playlist = await this.removeLatestAddition(playlist);
+        playlist = await this.sortByFrequency(playlist);
+        playlist = await this.getSuggestions(playlist);
+        return playlist;
+    }
+    //A method to give edit out suggestions for the oldest songs in the playlist based on what is most listened to all time
+    static async mostEditOutOld(lists){
+        let spotifyPlayList = lists[0];
+        let lastfm = lists[1];
+        let playlist = await this.getPlaylistFrequency(spotifyPlayList, lastfm);
+        playlist = await this.removeRecentAdditions(playlist);
+        playlist = await this.sortByMostFrequency(playlist);
+        playlist = await this.getSuggestions(playlist);
+        return playlist;
+    }
+    //A method to give edit out suggestions for the most recently added songs in the playlist based on what is most listened to all time
+    static async mostEditOutRecent(lists){
+        let spotifyPlayList = lists[0];
+        let lastfm = lists[1];
+        let playlist = await this.getPlaylistFrequency(spotifyPlayList, lastfm);
+        playlist = await this.removeLatestAddition(playlist);
+        playlist = await this.sortByMostFrequency(playlist);
+        playlist = await this.getSuggestions(playlist);
+        return playlist;
+    }
+    //A method to give edit out suggestions for the oldest songs in the playlist based on what is most listened in the past month
+    static async mostOneEditOutOld(lists){
+        let spotifyPlayList = lists[0];
+        let lastfm = lists[1];
+        let playlist = await this.getPlaylistFrequency(spotifyPlayList, lastfm);
+        playlist = await this.removeRecentAdditions(playlist);
+        playlist = await this.sortByMostFrequency(playlist);
+        playlist = await this.getSuggestions(playlist);
+        return playlist;
+    }
+    //A method to give edit out suggestions for the most recently added songs in the playlist based on what is most listened to in the past month
+    static async mostOneEditOutRecent(lists){
+        let spotifyPlayList = lists[0];
+        let lastfm = lists[1];
+        let playlist = await this.getPlaylistFrequency(spotifyPlayList, lastfm);
+        playlist = await this.removeLatestAddition(playlist);
+        playlist = await this.sortByMostFrequency(playlist);
+        playlist = await this.getSuggestions(playlist);
+        return playlist;
     }
 }
 
--- client/src/components/LoginServices.vue
+++ client/src/components/LoginServices.vue
@@ -1,14 +1,85 @@
 <template>
   <div>
-        <label for="lastfmAccount">LastFM Account: </label><br>
-        <input type="text" id="lastfmAccount" name="lastfmAccount" v-model="lastfmUsername"><br>
-        <label for="spotifyPlaylistLink">Spotify Playlist Link: </label><br>
-        <input type="text" id="spotifyPlaylistLink" name="spotifyPlaylistLink" v-model="spotifyPlaylistLink"><br>
-        <button v-on:click="track">Submit</button> <br />
-        <div v-for="(song, index) in songDeletions" :key="index">
-          <h1>Songs</h1>
-          <p>{{ song }}</p>
+        <div>
+          <div class="row center">
+            <label for="lastfmAccount">LastFM Account: </label><br />
+            <input type="text" id="lastfmAccount" name="lastfmAccount" v-model="lastfmUsername"><br />  
+          </div>
+          <br> 
+          <div class="row center">          
+            <label for="spotifyPlaylistLink">Spotify Playlist Link: </label><br />
+            <input class="increaseWidth" type="text" id="spotifyPlaylistLink" name="spotifyPlaylistLink" v-model="spotifyPlaylistLink"><br />
+          </div> 
+          <br>
+          <div class="row center">          
+            <button class="increaseSize" v-on:click="track">Submit</button> <br />
+          </div> 
         </div>
+
+        <div class="row">
+          <h1>Least Played in Past Month</h1>          
+          <div class="column left" style="background-color:#aaa;">
+            <h2>Oldest Songs in your Playlist</h2>
+            <div v-for="(song, index) in oneEditOld" :key="index">
+              <p>{{ `Song Name: ${song[0]}` }}<br> {{`Amount Played: ${song[2]}`}}</p>
+            </div>
+          </div>
+          <div class="column right" style="background-color:#bbb;">
+            <h2>Recently Added Songs in your Playlist</h2>
+            <div v-for="(song, index) in oneEditRecent" :key="index">
+              <p>{{ `Song Name: ${song[0]}` }}<br> {{`Amount Played: ${song[2]}`}}</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <h1>Leasted Played All-Time</h1>
+          <div class="column left" style="background-color:#aaa;">
+            <h2>Oldest Songs in Your Playlist</h2>
+            <div v-for="(song, index) in allTimeEditOld" :key="index">
+              <p>{{ `Song Name: ${song[0]}` }}<br> {{`Amount Played: ${song[2]}`}}</p>
+            </div>
+          </div>
+          <div class="column right" style="background-color:#bbb;">
+            <h2>Recently Added Songs in your Playlist</h2>
+            <div v-for="(song, index) in allTimeEditRecent" :key="index">
+              <p>{{ `Song Name: ${song[0]}` }}<br> {{`Amount Played: ${song[2]}`}}</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <h1>Most Played in Past Month</h1>          
+          <div class="column left" style="background-color:#aaa;">
+            <h2>Oldest Songs in Your Playlist</h2>
+            <div v-for="(song, index) in mostOneEditOld" :key="index">
+              <p>{{ `Song Name: ${song[0]}` }}<br> {{`Amount Played: ${song[2]}`}}</p>
+            </div>
+          </div>
+          <div class="column right" style="background-color:#bbb;">
+            <h2>Recently Added Songs in your Playlist</h2>
+            <div v-for="(song, index) in mostOneEditRecent" :key="index">
+              <p>{{ `Song Name: ${song[0]}` }}<br> {{`Amount Played: ${song[2]}`}}</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <h1>Most Played All-Time</h1>
+          <div class="column left" style="background-color:#aaa;">
+            <h2>Oldest Songs in Your Playlist</h2>
+            <div v-for="(song, index) in mostAllTimeEditOld" :key="index">
+              <p>{{ `Song Name: ${song[0]}` }}<br> {{`Amount Played: ${song[2]}`}}</p>
+            </div>
+          </div>
+          <div class="column right" style="background-color:#bbb;">
+            <h2>Recently Added Songs in your Playlist</h2>
+            <div v-for="(song, index) in mostAllTimeEditRecent" :key="index">
+              <p>{{ `Song Name: ${song[0]}` }}<br> {{`Amount Played: ${song[2]}`}}</p>
+            </div>
+          </div>
+        </div>
+
   </div>
 </template>
 
@@ -18,10 +89,16 @@
     name: 'LoginServices',
     data() {
       return {
-        songDeletions: [],
-        songAdditions: [],
+        allTimeEditOld: [],
+        allTimeEditRecent: [],
+        oneEditOld: [],
+        oneEditRecent: [],
+        mostAllTimeEditOld: [],
+        mostAllTimeEditRecent: [],
+        mostOneEditOld: [],
+        mostOneEditRecent: [],
         lastfmUsername: "allan-cao91",
-        spotifyPlaylistLink: "https://open.spotify.com/playlist/0Pe4oocsnswRI4v6zPTIwK?si=c806af560256477e",
+        spotifyPlaylistLink: "https://open.spotify.com/playlist/5xGyD6yI1owfzvSdo27Pqf?si=e57c581e2cf44dce",
       }
     },
     methods: {
@@ -38,12 +115,92 @@
         this.spotifyPlaylistLink = "";
       },
       async track() {
-        let result = await LastFMService.makeSuggestions(this.lastfmUsername, this.spotifyPlaylistLink);
-        this.songDeletions = result[0];
-        this.songAdditions = result[1];
-        
+        //Get a user's total list of songs from LastFM
+        let monthLists = await LastFMService.getPastMonthLists(this.lastfmUsername, this.spotifyPlaylistLink);
+        let allTimeLists = await LastFMService.getAllTimeLists(this.lastfmUsername, this.spotifyPlaylistLink);
+
+        //Create deep copies of these lists so the long API call doesn't need to be ran every time.
+        let oldLeastMonthLists = structuredClone(monthLists);
+        let recentLeastMonthList = structuredClone(monthLists);
+        let oldLeastAllTimeLists = structuredClone(allTimeLists);
+        let recentLeastAllTimeLists = structuredClone(allTimeLists);
+
+        let oldMostMonthLists = structuredClone(monthLists);
+        let recentMostMonthList = structuredClone(monthLists);
+        let oldMostAllTimeLists = structuredClone(allTimeLists);
+        let recentMostAllTimeLists = structuredClone(allTimeLists);
+
+        /*Suggest the playlist edits. Gives the edits for least listened songs in the past
+        month, in the oldest/most recently added songs in the playlist*/
+        let pastOld = await LastFMService.oneEditOutOld(oldLeastMonthLists);
+        this.oneEditOld = pastOld;
+        let pastRecent = await LastFMService.oneEditOutRecent(recentLeastMonthList);
+        this.oneEditRecent = pastRecent;
+        /*Suggest the playlist edits. Gives the edits for least listened songs all time, 
+        in the oldest/most recently added songs in the playlist*/
+        let old = await LastFMService.editOutOld(oldLeastAllTimeLists);
+        this.allTimeEditOld = old;
+        let recent = await LastFMService.editOutRecent(recentLeastAllTimeLists);
+        this.allTimeEditRecent = recent;
+        /*Suggest the playlist edits. Gives the edits for most listened songs in the past
+        month, in the oldest/most recently added songs in the playlist*/
+        let mostOneOld = await LastFMService.mostOneEditOutOld(oldMostMonthLists);
+        this.mostOneEditOld = mostOneOld;
+        let mostOneRecent = await LastFMService.mostOneEditOutRecent(recentMostMonthList);
+        this.mostOneEditRecent = mostOneRecent;
+        /*Suggest the playlist edits. Gives the edits for most listened songs 
+        all time, in the oldest/most recently added songs in the playlist*/
+        let mostOld = await LastFMService.mostEditOutOld(oldMostAllTimeLists);
+        this.mostAllTimeEditOld = mostOld;
+        let mostRecent = await LastFMService.mostEditOutRecent(recentMostAllTimeLists);
+        this.mostAllTimeEditRecent = mostRecent;
       }
       
     }
 }
 </script>
+
+<style>
+
+.center {
+  display: flex;
+  justify-content: center;
+}
+
+.divide {
+  padding: 5%;
+}
+
+.increaseWidth {
+  width: 30%;
+}
+
+.increaseSize {
+  width: 10%;
+  height: 10%;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+/* Create two unequal columns that floats next to each other */
+.column {
+  float: left;
+}
+
+.left {
+  width: 50%;
+}
+
+.right {
+  width: 50%;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+</style>
